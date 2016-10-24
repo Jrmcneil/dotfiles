65845f841a9084842818f0d57cb73491d628f454
James McNeil <jamesmcneil@Jamess-MacBook-Pro.local>
2016-10-24 16:47:58 +0100

    adds oh my zsh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc gitignore_global bashrc bash_profile vimrc lispwords ideavimrc tmux.conf"    # list of files/folders to symlink in homedir
+files="zshrc oh-my-zsh gitignore_global bashrc bash_profile vimrc lispwords ideavimrc tmux.conf"    # list of files/folders to symlink in homedir
 ##########
 
 # create dotfiles_old in homedir
